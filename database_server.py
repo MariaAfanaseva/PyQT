537f2ffe8a7fd2ff608016e05ddc3d4e528a5141
Maria Afanaseva <mashenkachukina@gmail.com>
2019-10-29 16:50:21 +0100

    Update database, added key and password hash

--- database_server.py
+++ database_server.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from common.variables import *
@@ -13,18 +13,19 @@ class ServerDB:
         id = Column(Integer, primary_key=True)
         login = Column(String, unique=True)
         fullname = Column(String)
-        password = Column(String)
+        password_hash = Column(String)
         last_login = Column(DateTime)
+        pubkey = Column(Text)
 
-        def __init__(self, login, fullname=None, password=None):
+        def __init__(self, login, fullname=None, password_hash=None, pubkey=None):
             self.login = login
             self.fullname = fullname
-            self.password = password
+            self.password_hash = password_hash
             self.last_login = datetime.datetime.now()
 
         def __repr__(self):
-            return "<User('%s, '%s', '%s')>" % \
-                   (self.login, self.fullname, self.password)
+            return "<User('%s, '%s', '%s, '%s')>" % \
+                   (self.login, self.fullname, self.password_hash, self.pubkey)
 
     class ActiveUsers(Base):
         __tablename__ = 'users_active'
@@ -93,35 +94,31 @@ class ServerDB:
                    (self.user_id, self.send, self.accepted)
 
     def __init__(self, path):
-        # Создаём движок базы данных
-        # SERVER_DATABASE - sqlite:///server_base.db3
-        # echo=False - отключение ведение лога (вывод sql-запросов)
-        # pool_recycle - По умолчанию соединение с БД через 8 часов простоя обрывается.
-        # Чтобы это не случилось нужно добавить опцию pool_recycle = 7200 (переуст-ка соед-я через 2 часа)
-        self.database_engine = create_engine(f'sqlite:///{path}', echo=False, pool_recycle=7200, connect_args={'check_same_thread': False})
+        # echo=False - disable logging (output sql queries)
+        # pool_recycle - By default, the connection to the database is terminated after 8 hours of inactivity.
+        # pool_recycle = 7200 - reconnect after 2 hours
+        self.database_engine = create_engine(f'sqlite:///{path}', echo=False, pool_recycle=7200,
+                                             connect_args={'check_same_thread': False})
 
         Base.metadata.create_all(self.database_engine)
 
         Session = sessionmaker(bind=self.database_engine)
         self.session = Session()
 
-        # Если в таблице активных пользователей есть записи, то их необходимо удалить
-        # Когда устанавливаем соединение, очищаем таблицу активных пользователей
+        # When we establish a connection, we clear the table of active users
         self.session.query(self.ActiveUsers).delete()
         self.session.commit()
 
-    def login_user(self, login, ip_address, port, fullname=None, password=None):
+    def login_user(self, login, ip_address, port, password_hash, key, fullname=None):
         user = self.session.query(self.AllUsers).filter_by(login=login)
         if user.count():
             user = user.first()
             user.last_login = datetime.datetime.now()
+            if user.pubkey != key:
+                user.pubkey = key
         else:
-            user = self.AllUsers(login, fullname, password)
-            self.session.add(user)
-            self.session.commit()
+            raise ValueError('Пользователь не зарегистрирован.')
 
-            add_in_history = self.UsersHistory(user.id)
-            self.session.add(add_in_history)
         now_time = datetime.datetime.now()
 
         active_user_new = self.ActiveUsers(user.id, ip_address, port, now_time)
@@ -131,10 +128,75 @@ class ServerDB:
         self.session.add(new_history)
         self.session.commit()
 
+    def add_user(self, login, password_hash, fullname=None):
+        user = self.AllUsers(login, fullname, password_hash)
+        self.session.add(user)
+        self.session.commit()
+
+        add_in_history = self.UsersHistory(user.id)
+        self.session.add(add_in_history)
+        self.session.commit()
+
+    def remove_user(self, login):
+        user = self.session.query(self.AllUsers).filter_by(login=login).first()
+        self.session.query(self.ActiveUsers).filter_by(user_id=user.id).delete()
+        self.session.query(self.HistoryLogin).filter_by(user_id=user.id).delete()
+        self.session.query(self.ContactsUsers).filter_by(user_id=user.id).delete()
+        self.session.query(self.UsersHistory).filter_by(user_id=user.id).delete()
+        self.session.query(self.AllUsers).filter_by(login=login).delete()
+        self.session.commit()
+
+    def get_hash(self, login):
+        user = self.session.query(self.AllUsers).filter_by(login=login).first()
+        return user.password_hash
+
+    def get_pubkey(self, login):
+        user = self.session.query(self.AllUsers).filter_by(login=login).first()
+        return user.pubkey
+
     def user_logout(self, login):
         user = self.session.query(self.AllUsers).filter_by(login=login).first()
         self.session.query(self.ActiveUsers).filter_by(user_id=user.id).delete()
         self.session.commit()
+
+    def add_contact(self, user, contact):
+        user = self.session.query(self.AllUsers).filter_by(login=user).first()
+        contact = self.session.query(self.AllUsers).filter_by(login=contact).first()
+
+        if not contact or self.session.query(self.ContactsUsers).filter_by(user_id=user.id, contact=contact.id).count():
+            return
+
+        new_contact = self.ContactsUsers(user_id=user.id, contact=contact.id)
+        self.session.add(new_contact)
+        self.session.commit()
+
+    def delete_contact(self, user, contact):
+        user = self.session.query(self.AllUsers).filter_by(login=user).first()
+        contact = self.session.query(self.AllUsers).filter_by(login=contact).first()
+
+        if not contact:
+            return
+
+        self.session.query(self.ContactsUsers).filter_by(user_id=user.id,
+                                                         contact=contact.id).delete()
+        self.session.commit()
+
+    def sending_message(self, sender, receiver):
+        sender_id = self.session.query(self.AllUsers).filter_by(login=sender).first().id
+        receiver_id = self.session.query(self.AllUsers).filter_by(login=receiver).first().id
+        sender_send = self.session.query(self.UsersHistory).filter_by(user_id=sender_id).first()
+        sender_send.send += 1
+        receiver_acc = self.session.query(self.UsersHistory).filter_by(user_id=receiver_id).first()
+        receiver_acc.accepted += 1
+        self.session.commit()
+
+    def get_contacts(self, login_user):
+        user = self.session.query(self.AllUsers).filter_by(login=login_user).one()
+        contacts = self.session.query(self.ContactsUsers, self.AllUsers.login).\
+            filter_by(user_id=user.id).\
+            join(self.AllUsers, self.ContactsUsers.contact == self.AllUsers.id)
+        contacts = [contact[1] for contact in contacts.all()]
+        return contacts
 
     def users_active_list(self):
         users = self.session.query(
@@ -163,45 +225,6 @@ class ServerDB:
             self.AllUsers.last_login
         )
         return users.all()
-
-    def add_contact(self, user, contact):
-        user = self.session.query(self.AllUsers).filter_by(login=user).first()
-        contact = self.session.query(self.AllUsers).filter_by(login=contact).first()
-
-        if not contact or self.session.query(self.ContactsUsers).filter_by(user_id=user.id, contact=contact.id).count():
-            return
-
-        new_contact = self.ContactsUsers(user_id=user.id, contact=contact.id)
-        self.session.add(new_contact)
-        self.session.commit()
-
-    def delete_contact(self, user, contact):
-        user = self.session.query(self.AllUsers).filter_by(login=user).first()
-        contact = self.session.query(self.AllUsers).filter_by(login=contact).first()
-
-        if not contact:
-            return
-
-        self.session.query(self.ContactsUsers).filter_by(user_id=user.id,
-                                                         contact=contact.id).delete()
-        self.session.commit()
-
-    def get_contacts(self, login_user):
-        user = self.session.query(self.AllUsers).filter_by(login=login_user).one()
-        contacts = self.session.query(self.ContactsUsers, self.AllUsers.login).\
-            filter_by(user_id=user.id).\
-            join(self.AllUsers, self.ContactsUsers.contact == self.AllUsers.id)
-        contacts = [contact[1] for contact in contacts.all()]
-        return contacts
-
-    def sending_message(self, sender, receiver):
-        sender_id = self.session.query(self.AllUsers).filter_by(login=sender).first().id
-        receiver_id = self.session.query(self.AllUsers).filter_by(login=receiver).first().id
-        sender_send = self.session.query(self.UsersHistory).filter_by(user_id=sender_id).first()
-        sender_send.send += 1
-        receiver_acc = self.session.query(self.UsersHistory).filter_by(user_id=receiver_id).first()
-        receiver_acc.accepted += 1
-        self.session.commit()
 
     def history_message(self):
         history = self.session.query(self.AllUsers.login,
