f4f1d0b7c914b4a547f03756d14c63e825c6256a
Maria Afanaseva <mashenkachukina@gmail.com>
2019-11-14 09:42:01 +0100

    Updated filters algorit

--- client/gui_client/gui_image.py
+++ client/gui_client/gui_image.py
@@ -1,5 +1,6 @@
 import sys
-from PIL import Image, ImageDraw
+import numpy as np
+from PIL import Image, ImageDraw, ImageOps
 from PIL.ImageQt import ImageQt
 from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 from PyQt5.QtGui import QPixmap
@@ -25,77 +26,57 @@ class ImageAddForm(QDialog):
 
     def open_image(self):
         self.path = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
-        pixmap = QPixmap(self.path)
-        pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
-        self.user_interface.imageLabel.setPixmap(pixmap)
-
-    def load_img(self):
-        image = Image.open(self.path)
-        draw = ImageDraw.Draw(image)
-        width = image.size[0]
-        height = image.size[1]
-        pix = image.load()
-        return image, draw, width, height, pix
+        self.pixmap = QPixmap(self.path)
+        self.pixmap_size = self.pixmap.scaled(300, 300, Qt.KeepAspectRatio)
+        self.user_interface.imageLabel.setPixmap(self.pixmap_size)
 
     def convert_img_show(self, image):
         img_tmp = ImageQt(image.convert('RGBA'))
-        pixmap = QPixmap.fromImage(img_tmp)
-        pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
-        self.user_interface.imageLabel.setPixmap(pixmap)
+        convert_pixmap = QPixmap.fromImage(img_tmp)
+        convert_pixmap_size = convert_pixmap.scaled(300, 300, Qt.KeepAspectRatio)
+        self.user_interface.imageLabel.setPixmap(convert_pixmap_size)
 
     def set_sepia(self):
-        image, draw, width, height, pix = self.load_img()
-        depth = 30
-        for i in range(width):
-            for j in range(height):
-                a = pix[i, j][0]
-                b = pix[i, j][1]
-                c = pix[i, j][2]
-                S = (a + b + c)
-                a = S + depth * 2
-                b = S + depth
-                c = S
-                if (a > 255):
-                    a = 255
-                if (b > 255):
-                    b = 255
-                if (c > 255):
-                    c = 255
-                draw.point((i, j), (a, b, c))
+        """
+        Optimization on the sepia filter using numpy
+        """
 
-        self.convert_img_show(image)
+        image = Image.open(self.path)
+
+        # Load the image as an array so cv knows how to work with it
+        img = np.array(image)
+
+        # Apply a transformation where we multiply each pixel
+        # rgb with the matrix transformation for the sepia
+
+        lmap = np.matrix([[0.393, 0.769, 0.189],
+                          [0.349, 0.686, 0.168],
+                          [0.272, 0.534, 0.131]
+                          ])
+
+        filt = np.array([x * lmap.T for x in img])
+
+        # Check wich entries have a value greather than 255 and set it to 255
+        filt[np.where(filt > 255)] = 255
+
+        # Create an image from the array
+        self.convert_img_show(Image.fromarray(filt.astype('uint8')))
 
     def set_negative(self):
-        image, draw, width, height, pix = self.load_img()
-        depth = 30
-        for i in range(width):
-            for j in range(height):
-                a = pix[i, j][0]
-                b = pix[i, j][1]
-                c = pix[i, j][2]
-                draw.point((i, j), (255 - a, 255 - b, 255 - c))
-
-        self.convert_img_show(image)
+        img = Image.open(self.path)
+        img_invert = ImageOps.invert(img)
+        self.convert_img_show(img_invert)
 
     def set_gray(self):
-        image, draw, width, height, pix = self.load_img()
-        for i in range(width):
-            for j in range(height):
-                a = pix[i, j][0]
-                b = pix[i, j][1]
-                c = pix[i, j][2]
-                S = (a + b + c) // 3
-                draw.point((i, j), (S, S, S))
-
-        self.convert_img_show(image)
+        img = Image.open(self.path).convert('LA')
+        self.convert_img_show(img)
 
     def set_default(self):
-        pixmap = QPixmap(self.path)
-        pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
-        self.user_interface.imageLabel.setPixmap(pixmap)
+        self.user_interface.imageLabel.setPixmap(self.pixmap_size)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_window = ImageAddForm()
+    main_window.init_ui()
     sys.exit(app.exec_())
